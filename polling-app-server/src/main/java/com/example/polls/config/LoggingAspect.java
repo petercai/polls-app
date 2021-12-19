36df2b4e9878fbf8b1df7d0374bde7d94f3b12b7
Peter Cai <petercaica@hotmail.com>
2021-12-19 17:21:11 -0500

    BigQuery

---
+++ polling-app-server/src/main/java/com/example/polls/config/LoggingAspect.java
@@ -0,0 +1,69 @@
+package com.example.polls.config;
+
+import com.google.common.base.Stopwatch;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.util.ArrayList;
+
+@Aspect
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@Configuration
+public class LoggingAspect {
+
+    @Around(
+            "execution(public * com.example.polls.controller..*(..)) || "
+                    + "execution(public * com.example.polls.service..*(..)) ")
+    public Object log(final ProceedingJoinPoint point) throws Throwable {
+        Logger LOGGER = null;
+        String methodName = null;
+        Stopwatch timer = null;
+        Object retVal = null;
+        Object target = point.getTarget();
+        try {
+            if (target != null) {
+                LOGGER = LoggerFactory.getLogger(target.getClass());
+                timer = Stopwatch.createStarted();
+                methodName = ((MethodSignature) point.getSignature()).getMethod().getName();
+
+                Object[] args = point.getArgs();
+                String params = "";
+                if (args != null && args.length > 0) {
+                    ArrayList<String> list = new ArrayList<>(args.length);
+                    for (Object o : args) {
+                        list.add(String.valueOf(o));
+                    }
+                    params = String.join(",",
+                                         list);
+                }
+
+                LOGGER.info(String.format("ASPECT-start: %s(%s)",
+                                          methodName,
+                                          params));
+            }
+
+            retVal = point.proceed();
+            return retVal;
+        } catch (final Exception ex) {
+            LOGGER.error("ASPECT-Exception caught in LoggingAspect - ",
+                         ex);
+            throw ex;
+        } finally {
+            if (target != null) {
+                LOGGER.info(
+                        "ASPECT-end: "
+                                + methodName
+                                + "() in "
+                                + timer.stop().toString()
+                                + " with return = "
+                                + String.valueOf(retVal)); // $NON-NLS-1$
+            }
+        }
+    }
+}
---
+++ polling-app-server/src/main/java/com/example/polls/config/RequestLoggingConfig.java
@@ -0,0 +1,18 @@
+package com.example.polls.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+    @Bean
+    public CommonsRequestLoggingFilter loggingFilter() {
+        CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+        filter.setAfterMessagePrefix("Logging - Request: ");
+        filter.setIncludeQueryString(true);
+        filter.setIncludeHeaders(true);
+        filter.setIncludePayload(true);
+        return filter;
+    }
+}
